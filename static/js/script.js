ed50a7901156ceb87484e5d20bcb8bfa74332e97
Dioney Gabriel <62361235+SimpleDioney@users.noreply.github.com>
2025-07-08 14:54:17 -0300

    Add files via upload

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,11 @@
-console.log("Script.js carregado!");
+document.addEventListener('DOMContentLoaded', function() {
+    const deleteButtons = document.querySelectorAll('button.delete');
+    deleteButtons.forEach(button => {
+        button.addEventListener('click', function(event) {
+            const confirmation = confirm('Tem certeza que deseja excluir este item? Esta ação não pode ser desfeita.');
+            if (!confirmation) {
+                event.preventDefault();
+            }
+        });
+    });
+});
